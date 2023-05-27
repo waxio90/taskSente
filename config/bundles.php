5ff6febfb4cd0570dd5b61acc734d2c365b3ff0d
Wojciech Krupiński <krupinski1@gmail.com>
2023-05-27 22:59:43 +0200

    Poprawka Dockerfile, dodanie bootstrapa

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
