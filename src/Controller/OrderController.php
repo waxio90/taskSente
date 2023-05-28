15a6a950c4a2b26b4a486c310d94c5c9a0d1f5fa
Wojciech Krupiński <krupinski1@gmail.com>
2023-05-28 21:32:10 +0200

    Dodanie sortowania Controller Service Front

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,29 +3,52 @@
 namespace App\Controller;
 
 use App\Service\OrderService;
+use App\Service\SortService;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+    private array $ordersData;
+
+    /**
+     * @throws Exception
+     */
     public function __construct(
-        private readonly OrderService $orderService
+        private readonly OrderService $orderService,
+        private readonly SortService $sortService
     )
     {
+        $this->ordersData = $this->orderService->loadOrdersFromJson();
     }
 
     /**
      * @throws Exception
      */
-    #[Route('/', name: 'list_order')]
-    public function listOrder(): Response
+    #[Route('/', name: 'list_orders')]
+    public function listOrders(): Response
     {
-        $orderData = $this->orderService->loadOrdersFromJson();
+        return $this->render('orders/index.html.twig', [
+            'orders' => $this->ordersData,
+        ]);
+    }
 
-        return $this->render('order/index.html.twig', [
-            'orders' => $orderData,
+    #[Route('/{name}', name: 'sort_list_orders')]
+    public function sortListOrders(Request $request): Response
+    {
+        $sortParameters = [
+            'sort_column' => $request->get('sort_column'),
+            'sort_direction' => $request->get('sort_direction')
+        ];
+
+        $ordersData = $this->sortService->sortOrdersByParameters($sortParameters, $this->ordersData);
+
+        return $this->render('orders/index.html.twig', [
+            'orders' => $ordersData,
+            'sort' => $sortParameters,
         ]);
     }
 }
---
+++ src/Service/SortService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Service;
+
+use Exception;
+
+class SortService
+{
+    public function sortOrdersByParameters(array $sortParameters, array $listOrder): array
+    {
+        try {
+            return $this->sortingListOrders($sortParameters, $listOrder);
+        } catch (Exception $e) {
+            print $e->getMessage();
+            return [];
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function sortingListOrders(array $sortParameters, array $orders): array
+    {
+        $sortColumn = $sortParameters['sort_column'];
+        $sortDirection = $sortParameters['sort_direction'];
+        $this->checkParametersToSorting($sortColumn,$sortDirection, $orders);
+
+        usort($orders, function ($a, $b) use ($sortColumn, $sortDirection) {
+            $comparison = $a[$sortColumn] <=> $b[$sortColumn];
+
+            if ($sortDirection === 'desc') {
+                $comparison *= -1;
+            }
+            return $comparison;
+        });
+        return $orders;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function checkParametersToSorting(string $sortColumn, string $sortDirection, array $orders): void
+    {
+        if (empty($sortColumn) || empty($sortDirection)) {
+            throw new Exception('Podano błędne parametry sortowania');
+        }
+        if (!in_array($sortColumn, array_keys($orders[0]))) {
+            throw new Exception('Podana kolumna nie występuje w tabeli zamówień');
+        }
+        if ($sortDirection !== 'desc' && $sortDirection !== 'asc') {
+            throw new Exception('Podano błędny kierunek sortowania');
+        }
+    }
+}
